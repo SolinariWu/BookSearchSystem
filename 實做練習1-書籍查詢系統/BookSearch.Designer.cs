1260165200d7a1191d3096afc37656619ff2f806
WuChengYu <u0124046@nkfust.edu.tw>
2015-10-03 11:54:03 +0800

    實做練習1

---
+++ 實做練習1-書籍查詢系統/BookSearch.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.result = new System.Windows.Forms.TextBox();
+            this.search = new System.Windows.Forms.Button();
+            this.searchISDN = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.booklist = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(570, 262);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.result);
+            this.tabPage1.Controls.Add(this.search);
+            this.tabPage1.Controls.Add(this.searchISDN);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(562, 236);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "查詢";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // result
+            // 
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.result.Enabled = false;
+            this.result.Location = new System.Drawing.Point(6, 29);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(469, 78);
+            this.result.TabIndex = 2;
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(147, 0);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(75, 23);
+            this.search.TabIndex = 1;
+            this.search.Text = "查詢";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // searchISDN
+            // 
+            this.searchISDN.Location = new System.Drawing.Point(0, 0);
+            this.searchISDN.Name = "searchISDN";
+            this.searchISDN.Size = new System.Drawing.Size(141, 22);
+            this.searchISDN.TabIndex = 0;
+            this.searchISDN.Text = "請輸入ISDN，為4碼數字";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.booklist);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(562, 236);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "書籍清單";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // booklist
+            // 
+            this.booklist.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.booklist.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.booklist.Location = new System.Drawing.Point(3, 3);
+            this.booklist.Multiline = true;
+            this.booklist.Name = "booklist";
+            this.booklist.ReadOnly = true;
+            this.booklist.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.booklist.Size = new System.Drawing.Size(556, 230);
+            this.booklist.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0124046吳承諭";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0124046吳承諭";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 262);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "0124046吳承諭書籍查詢系統";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox booklist;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.TextBox searchISDN;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 實做練習1-書籍查詢系統/BookSearch.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+    public partial class Form1 : Form
+    {
+        
+             BOOK[] BookList = new BOOK[15];
+           
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BookList[0] = new BOOK(3654, "高第一簡介", "陳振遠", "高第一出版社", category.其它, 250);
+            BookList[1] = new BOOK(6391, "系統分析與設計", "黃文楨", "高第一出版社", category.科學, 200);
+            BookList[2] = new BOOK(2269, "管理學概論", "周棟詳", "資管出版社", category.商管, 100);
+            BookList[3] = new BOOK(9541, "哈利波特", "JK羅琳", "皇冠出版社", category.小說, 450);
+            BookList[4] = new BOOK(2301, "迷霧之子", "山德森", "Tor Books", category.小說, 550);
+            BookList[5] = new BOOK(1265, "台灣歷史", "孫博士", "年遠出版社", category.歷史, 150);
+            BookList[6] = new BOOK(8851, "商業週刊", "蔡明德", "商業出版社", category.商管, 50);
+            BookList[7] = new BOOK(3302, "龍槍編年史", "西克曼", "奇幻基地", category.小說, 400);
+            BookList[8] = new BOOK(9993, "世界史概論", "王明那", "年遠出版社", category.歷史, 80);
+            BookList[9] = new BOOK(4567, "股票理論", "王董", "賺錢出版社", category.商管, 250);
+            BookList[10] = new BOOK(3336, "三十三年夢", "朱天心", "印刻出版社", category.文學, 100);
+            BookList[11] = new BOOK(5541, "解讀台灣的經濟", "吳私文", "經管出版社", category.商管, 250);
+            BookList[12] = new BOOK(3985, "兩代人的十二月", "陳振遠", "高第一出版社", category.其它, 250);
+            BookList[13] = new BOOK(1009, "旅行式一場修行", "林輝", "時報出版社", category.文學, 330);
+            BookList[14] = new BOOK(1985, "程式設計流程", "賈伯斯", "蘋果出版社", category.科學, 750);
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            booklist.Text = "ISDN  \t 書名\t\t作者\t出版社\t\t類型\t售價 " + Environment.NewLine;
+            booklist.Text += "-------------------------------------------------------------" + Environment.NewLine; ;
+            for (int i = 0; i <= 14; i++)
+            {
+                booklist.Text += BookList[i].isdn + "\t" + BookList[i].name + "\t" + BookList[i].auth + "\t" + BookList[i].pub
+                    + "\t" + BookList[i].cate + "\t" + BookList[i].pri + Environment.NewLine;
+            }
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            int isdnhave = 0;//判斷是否有這本書
+           int  i = 0;//判斷書入是否為數字的變數
+            bool judgenumber = int.TryParse(searchISDN.Text,out i);
+            if (i == 0)
+            {
+                MessageBox.Show("請輸入數字!");
+                searchISDN.Text = "請輸入ISDN，為四碼數字";
+            }
+            else if (searchISDN.Text.ToString().Length == 4)
+            {
+
+                for (int k = 0; k <= 14; k++)
+                {
+                    if (BookList[k].isdn == int.Parse(searchISDN.Text))
+                    {
+                        result.Text = "ISDN \t書名\t\t作者\t出版社\t\t類型\t售價 " + Environment.NewLine;
+                        result.Text += "-----------------------------------------------------------------------------------------" + Environment.NewLine; ;
+                        result.Text += BookList[k].isdn + "\t" + BookList[k].name + "\t" + BookList[k].auth + "\t" + BookList[k].pub
+                    + "\t" + BookList[k].cate + "\t" + BookList[k].pri;
+                        isdnhave  = 1;
+                        break;
+                    }
+
+                }
+                if (isdnhave == 0)
+                {
+                    result.Text = "查無此ISDN，請至書籍清單查詢!";
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入4碼數字!");
+                searchISDN.Text = "請輸入ISDN，為四碼數字";
+            }
+        }
+
+        
+    }
+    public enum category 
+    {
+        科學 ,
+        小說 ,
+        商管, 
+        歷史 ,
+        文學 ,
+        其它
+    }
+    class BOOK
+    {
+        public int isdn;
+        public string name;
+        public string auth;
+        public string pub;
+        public category cate;
+        public int pri;
+        public BOOK(int ISDN, string BookName, string Author ,string Publisher,category cate,int Price)
+    {
+        this.isdn = ISDN;
+        this.name = BookName;
+        this.auth = Author;
+        this.pub = Publisher;
+        this.cate = cate;
+        this.pri = Price;
+    }
+    }
+}
